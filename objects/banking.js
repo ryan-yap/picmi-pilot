c5efea8da14a12f2ddbae44464675019f6e7a263
Kang Shiang <ykshiang@alumni.ubc.ca>
2015-03-11 15:14:33 -0700

    added new files

---
+++ objects/banking.js
@@ -0,0 +1,17 @@
+var user_db = require('mongoskin').db('mongodb://52.11.66.117:27017/User');
+
+function Banking(payment_method) {
+	this.payment_method = payment_method;
+}
+
+Banking.prototype.insert = function (id) {
+	this._id = id;
+	user_db.collection('banking').insert(this, function(err, result) {
+	  	if (err){ 
+	  		throw err; 
+	  	}
+	  	console.log(result[0])
+	});
+}
+
+module.exports = Banking;
