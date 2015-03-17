3d8f4303bccacd0b4e71a9757015daa72fa93114
Kang Shiang <ykshiang@alumni.ubc.ca>
2015-03-17 12:28:14 -0700

    unable to put a file yet

--- routes/photos.js
+++ routes/photos.js
@@ -3,31 +3,40 @@ var passport = require('../passports/passport.js');
 var router = express.Router();
 var Photo = require('../objects/photo')
 var AWS = require('aws-sdk'); 
-AWS.config.region = 'us-west-2';
 var ObjectID = require('mongoskin').ObjectID
 var s3 = new AWS.S3(); 
 
-AWS.config.update({
-    accessKeyId: "AKIAI2MWE2V5KMIVBKGA",
-    secretAccessKey: "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"
-});
+// AWS.config.update({
+//     accessKeyId: "AKIAICVKGCDJ6XYSVS6Q",
+//     secretAccessKey: "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",
+//     //"region": "us-west-1"
+// });
 
 /* GET users listing. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-	console.log(req.param)
+	var params = {
+ 	 	Bucket: 'picmi-photo', /* required */
+  		//Key: 'photo/', /* required */
+	};
+	// s3.getSignedUrl('getObject', params, function (err, url) {
+ //  		console.log("error:",err)
+ //  		console.log(url)
+	// });
+	s3.listObjects(params, function(err, data) {
+	  if (err) console.log(err, err.stack); // an error occurred
+	  else     console.log(data);           // successful response
+	});
+
+	res.send("Photo");
 });
 
 router.get('/upload', ensureAuthenticated, function(req, res, next){
-	AWS.config.update({
-    accessKeyId: "AKIAI2MWE2V5KMIVBKGA",
-    secretAccessKey: "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"
-});
-	var html_temp = '<form action="UPLOADURL"><input type="file" name="pic" accept="image/*"><input type="submit"></form>'
-	var params = {Bucket: 'picmi-photo', Key: 'test'};
+	var html_temp = '<form action="UPLOADURL" method="post" enctype="multipart/form-data"><input type="file" name="pic" accept="image/*"><input type="submit"></form>'
+	var params = {Bucket: 'picmi-photo', Key: 'photo/', Expires: 3600, ACL: "public-read", ContentType: "image/jpeg"};
 	s3.getSignedUrl('putObject', params, function (err, url) {
-  		console.log('The URL is', err);
+  		console.log(err);
   		var html = html_temp.replace("UPLOADURL", url);
-		console.log(html)
+		console.log(url)
 		res.send(html)
 	});
 })
