489f88c395e4a85f584ec38b4e70e05919f0de2e
Ryan <ykshiang@gmail.com>
2015-04-01 12:57:52 -0700

    WIP

---
+++ objects/device_token.js
@@ -0,0 +1,17 @@
+var user_db = require('mongoskin').db('mongodb://54.153.62.38:27017/User');
+
+function Device_Token(device_token) {
+	this.token = device_token;
+}
+
+Device_Token.prototype.insert = function (id) {
+	this._id = id;
+	user_db.collection('device_token').insert(this, function(err, result) {
+	  	if (err){ 
+	  		throw err; 
+	  	}
+	  	console.log(result[0])
+	});
+}
+
+module.exports = Device_Token;
